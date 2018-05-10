43a863acf5d0491ad05efedbbd38d5b474a01d4c
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2018-05-10 15:30:45 -0300

    Add files via upload
    Merge Sort inserido no algoritmo

--- PoD/t1/ordenacao.cpp
+++ PoD/t1/ordenacao.cpp
@@ -4,8 +4,69 @@
 
 /**************** ORDENAÇÃO INTERNA ****************/
 
+// Segundo vet aux (de meio+1 até fim)
+// Segundo vet aux (de meio+1 até fim)
+// Da merge dos dois sub vet no vet novamente
 
-// Usar merge para ordenação interna
+void merge (int vet[], int init, int meio, int fim) {
+  int pt1 = 1 + meio - init;
+  int pt2 = fim - meio;
+
+  int aux1[pt1], aux2[pt2];
+
+  for (int i = 0; i < pt1; i++) {
+    aux1[i] = vet[init + i];
+  }
+  for (int j = 0; j < pt2; j++) {
+    aux2[j] = vet[j + meio + 1];
+  }
+
+  // Da merge ja ordenando os vetores
+  // k -> pos do primeiro vet original
+  // i -> pos do primeiro vet aux
+  // j -> pos do segundo vet aux
+  int k = init, i = 0, j = 0;
+
+  while (i < pt1 && j < pt2) {
+    if (aux1[i] <= aux2[j]) {
+      vet[k] = aux1[i];
+      i++;
+    } else {
+      vet[k] = aux2[j];
+      j++;
+    }
+    k++;
+  }
+
+  // Termina de passar os elementos de aux1 pra vet
+  // Termina de passar os elementos de aux2 pra vet
+  while (i < pt1) {
+    vet[k] = aux1[i];
+    i++;
+    k++;
+  }
+  while (j < pt2) {
+    vet[k] = aux2[j];
+    j++;
+    k++;
+  }
+}
+
+
+// init = primeira metade do vet
+// fim = segunda metade do vet
+// Ordena as duas metades
+void mergeSort(int vet[], int init, int fim) {
+  if (init < fim) {
+    // Começa do init e vai pra metade do mesmo
+    int meio = init+(fim-init)/2;
+
+    mergeSort(vet, init, meio);
+    mergeSort(vet, meio+1, fim);
+
+    merge(vet, init, meio, fim);
+  }
+}
 
 
 /**************** MANIPULAÇÃO DE ARQUIVOS ****************/
--- PoD/t1/ordenacao.hpp
+++ PoD/t1/ordenacao.hpp
@@ -6,6 +6,12 @@ using namespace std;
 
 /** ORDENAÇÃO INTERNA **/
 
+void merge (int vet[], int init, int meio, int fim);
+
+void mergeSort(int vet[], int init, int fim);
+
+
+
 /** MANIPULAÇÃO DE ARQUIVOS **/
 
 fstream openFile (string name);
