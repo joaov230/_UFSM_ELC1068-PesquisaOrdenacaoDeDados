cfa3173812c27751b7bdd39367db4fe41b25f51a
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2018-05-14 16:45:53 -0300

    Add files via upload

--- PoD/t1/ordenacao.hpp
+++ PoD/t1/ordenacao.hpp
@@ -15,8 +15,6 @@ void mergeSort(int vet[], int init, int fim);
 
 /** MANIPULAÇÃO DE ARQUIVOS **/
 
-// void arquivoParaVetores (fstream f, string strChar, vector<int> vetInt);
-
 
 
 /** ORDENAÇÃO EXTERNA **/
