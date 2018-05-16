dfb97546ee0a5e76eba690ad545b7355e1d12a43
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2018-05-16 14:59:34 -0300

    Add files via upload
    Funcional e sem comentários desnecessários

--- PoD/t1/main.cpp
+++ PoD/t1/main.cpp
@@ -3,21 +3,5 @@
 int main (void) {
   externalSort("teste.txt");
 
-  // ifstream f;
-  // f.open("testando.txt");
-  // char c,a,b;
-  // f.get(a);
-  // f.get(b);
-  // f.get(c);
-  //
-  // cout << "A = " << a << endl;
-  // cout << "B = " << b << endl;
-  // cout << "C = " << c << endl;
-  //
-  //
-  // if (f.eof()) {
-  //   cout << "\n\nHAHA";
-  // }
-
   return 0;
 }
